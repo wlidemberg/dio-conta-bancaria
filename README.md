282f5da9dc67caf5e4e8409f27fcdfa333dcd483
Wlidemberg <automatizei@gmail.com>
2025-09-18 12:29:59 -0300

    Separação do código em funções e atualização do readme

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Simulação de funcionalidades basicas das operações de deposito e saque e dem
 ## 🎞 Demonstração
 
 ![alt text](image.png)
+
+## Atualizações
+
+ - Separação do codigo em funções como:
+    - data_hora_atual
+    - efetuar_deposito
+    - efetuar_saque
+ - cálculo de transações diarias baseadas em datas 
--- banco.py
+++ banco.py
@@ -1,3 +1,6 @@
+import datetime
+import pytz
+
 menu  = """
 [1] Depositar | [2] Sacar | [3] Extrato | [4] Sair\n
 Digite a operação Desejada: \n
@@ -6,23 +9,73 @@ Digite a operação Desejada: \n
 saldo = []
 depositos = []
 saques = []
+dt_transacoes = []
 saldo_atual = 0
 quantidade_saques = 0
+quantidades_transacoes_diaria = 0
+data_ultima_transacao = datetime.date.today()
 LIMITE_SAQUES = 3
 LIMITE_SAQUE = 500
+LIMITE_TRANSACAO_DIARIA = 5
 
+def data_hora_atual():
+  dt = datetime.datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d/%m/%Y %H:%M:%S')
+  return dt
+
+def efetuar_deposito(deposito:float):
+  global quantidades_transacoes_diaria, saldo_atual
+  if deposito > 0:
+    if quantidades_transacoes_diaria < LIMITE_TRANSACAO_DIARIA:
+      saldo.append(deposito)
+      dt_transacoes.append(data_hora_atual())
+      saldo_atual = sum(saldo)
+      quantidades_transacoes_diaria+=1 
+      return saldo_atual
+    else:
+      print(f"Você excedeu o limite de {LIMITE_TRANSACAO_DIARIA} por dia! Volte amanha!")
+      return saldo_atual
+  else:
+    print('Não é possivel depositos zerados ou com valores negativos. Tente Novamente!')
+    return saldo_atual
+
+def efetuar_saque(saque, quantidade_saques):
+  global quantidades_transacoes_diaria
+  if quantidades_transacoes_diaria < LIMITE_TRANSACAO_DIARIA:
+    if saque > 0 and saque <= LIMITE_SAQUE:
+      if quantidade_saques < LIMITE_SAQUES:
+        if saldo_atual > saque:
+          saques.append(saque)
+          dt_transacoes.append(data_hora_atual())
+          quantidades_transacoes_diaria +=1
+          return saque
+        else:
+          print('Saldo Insufuciente. Verifique valor disponível!')
+          return 0
+      else:
+        print('Quantidade de saques por dia ultrapassado, volte amanha para relizar saques.') 
+        return 0
+    else:
+      print(f'Valor do saque excedente, seu limite para saque é de R$ {LIMITE_SAQUE:.2f} por saque.')
+      return 0
+  else:
+    print(f"Você excedeu o limite de {LIMITE_TRANSACAO_DIARIA} por dia! Volte amanha!")
+    return 0
+      
 while True:
-
-
+  data_do_dia = datetime.date.today()
+  if data_do_dia > data_ultima_transacao:
+    # Novo dia - zeramos o contador de transações
+    quantidades_transacoes_diaria = 0
+    data_ultima_transacao = data_do_dia
 
   if saldo_atual > 0:
     print('\n')
     print('-'*100)
-    print(f'Saldo atual: R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques}')
+    print(f'Saldo atual: R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques} | Transações Dia {quantidades_transacoes_diaria} | Data: {data_do_dia}')
     print('-'*100)
   else:
     print('-'*100)
-    print(f'Saldo atual: R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques}')
+    print(f'Saldo atual: R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques} | Transações Dia {quantidades_transacoes_diaria} | Data: {data_do_dia}')
     print('-'*100)
 
   operacao = input(menu)
@@ -31,11 +84,9 @@ while True:
     print('========== DEPOSITAR ==========\n')
     try:
       deposito = float(input('Digite o valor a ser depositado: '))
-      if deposito > 0:
-        saldo.append(deposito)
-        saldo_atual = sum(saldo)
-      else:
-        print('Não é possivel depositos zerados ou com valores negativos. Tente Novamente!')
+      valor_transacao = efetuar_deposito(deposito)
+      saldo_atual = valor_transacao
+      
     except ValueError:
       print('Entrada inválida, Digite apenas números!', ValueError)
 
@@ -46,38 +97,30 @@ while True:
         print('Não há saldo suficiente para realizar saques.')
         continue
       saque = float(input("Digite o valor a ser sacado: "))
-      saques.append(saque)
-      if saque > 0 and saque <= LIMITE_SAQUE:
-        if quantidade_saques < LIMITE_SAQUES:
-          if saldo_atual >= saque:
-            saldo_atual = saldo_atual - saque
-            quantidade_saques += 1
-            print(f"Saque de R$ {saque:.2f} autorizado, ")
-          else:
-            print('Saldo Insufuciente. Verifique valor disponível!')
-        else:
-          print('Quantidade de saques por dia ultrapassado, volte amanha para relizar saques.')
-      else:
-        print(f'Valor do saque excedente, seu limite para saque é de R$ {LIMITE_SAQUE:.2f} por saque.')
+      dados_saque = efetuar_saque(saque, quantidade_saques)
+      saldo_atual = saldo_atual - dados_saque
+      quantidade_saques +=1 if quantidade_saques < 3 else 0  
+       
     except ValueError:
-      print('Entrada inválida, Digite apenas números!', ValueError)
+          print('Entrada inválida, Digite apenas números!', ValueError)
 
   elif operacao == '3':
     print('========== EXTRATO ==========\n')
     if len(saques) == 0 and len(saldo) == 0:
       print('Não houve movimentações nessa conta.')
       continue
-    print('Depositos --------------------------------------------')  
-    for id,  valor in enumerate(saldo):
-      print(f"{id+1:2} {'-'*20} [+] R$ {valor:8.2f}")
+    print('Depositos --------------------------------------------')
+      
+    for id, (valor, data) in enumerate(zip(saldo, dt_transacoes)):
+      print(f"{id+1:2} {'-'*20} [+] R$ {valor:8.2f} - {data}")
       
     print('\nSaques ---------------------------------------------') 
-    for id, valor in enumerate(saques):
-      print(f"{id+1:<2} {'-'*20} [-] R$ {valor:8.2f}")
+    for id, (valor, data) in enumerate(zip(saques, dt_transacoes)):
+      print(f"{id+1:<2} {'-'*20} [-] R$ {valor:8.2f} - {data}")
     
     print('-'*100)
     print('-'*100)
-    print(f'Saldo atual ------------[+] R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques}')
+    print(f'Saldo atual ------------[+] R$ {saldo_atual:.2f} | Saques realizados no dia {quantidade_saques} | Transações efetuadas: {quantidades_transacoes_diaria}')
     
   elif operacao == '4':
     print('Sair')
